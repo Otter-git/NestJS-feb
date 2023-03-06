741cfdfd0fffb4d89d86e518f0af0f2bf0416c79
Alice <000>
2023-03-07 00:38:10 +0100

    homework-6

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Render,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreateUserDto } from './dtos/create-user-dto';
+import { EditUserDto } from './dtos/edit-user-dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -9,5 +22,19 @@ export class UsersController {
   @Post('api')
   async create(@Body() user: CreateUserDto) {
     return this.userService.create(user);
+  }
+
+  @Get('edit-profile/:id')
+  @Render('edit-profile')
+  async renderEditProfile(@Param('id', ParseIntPipe) id: number, @Req() req) {
+    const _user = await this.userService.findById(id);
+    return _user;
+  }
+
+  @Patch('api')
+  @UseGuards(JwtAuthGuard)
+  async edit(@Body() user: EditUserDto, @Req() req) {
+    const jwtUserId = req.user.userId;
+    return this.userService.edit(jwtUserId, user);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { checkPermission, Modules } from 'src/auth/role/utils/check-permission';
 import { hash } from 'src/utils/crypto';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dtos/create-user-dto';
+import { EditUserDto } from './dtos/edit-user-dto';
 import { UsersEntity } from './users.entity';
 
 @Injectable()
@@ -20,6 +22,29 @@ export class UsersService {
     usersEntity.password = await hash(user.password);
 
     return this.usersRepository.save(usersEntity);
+  }
+
+  async edit(id: number, user: EditUserDto) {
+    const _user = await this.findById(id);
+    if (!_user) {
+      throw new HttpException(
+        {
+          status: HttpStatus.FORBIDDEN,
+          error: 'Неверный идентификатор пользователя',
+        },
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    _user.firstName = user.firstName || _user.firstName;
+    _user.email = user.email || _user.email;
+
+    if (checkPermission(Modules.changeRole, _user.roles)) {
+      _user.roles = user.roles || _user.roles;
+    }
+    _user.password = (await hash(user.password)) || _user.password;
+
+    return this.usersRepository.save(_user);
   }
 
   async findById(id: number) {
