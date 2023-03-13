eefd366447ee4c4c9481272b4c27b7fc5dfa5656
Alice <000>
2023-03-13 13:55:48 +0100

    homework-8(edited)

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { ConfigModule } from '@nestjs/config';
       host: process.env.DATABASE_HOST,
       port: parseInt(process.env.DATABASE_PORT),
       username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWOR,
+      password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
